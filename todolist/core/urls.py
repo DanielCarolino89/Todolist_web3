ab7ac4687bd87f4b43b643044317ba84f710fab3
Daniel de Godoy Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2023-05-06 21:24:41 -0300

    Delete todolist directory

--- todolist/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'core'
-
-urlpatterns = [
-    path('', views.verifica_atividade, name="index"),
-    path('cadastro', views.cadastro, name="cadastro")
-]
